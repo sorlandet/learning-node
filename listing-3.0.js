f4481e0a18a0d4495e4cdec6aae03378b9408325
Yevgeniy Medvedev <emedvedev@thumbtack.net>
2014-05-21 17:27:16 +0700

    adding few code samples

---
+++ listing-3.0.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Yevgeniy on 21.05.2014.
+ */
+
+
+
+//console.log(global);
+
+console.log(process.execPath);
+console.log(process.version);
+console.log(process.platform);
+
+console.log(process.memoryUsage());
---
+++ listing-3.1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Yevgeniy on 21.05.2014.
+ */
+
+process.stdin.resume();
+
+process.stdin.on('data', function(chunk) {
+    process.stdout.write('data: ' + chunk);
+})
