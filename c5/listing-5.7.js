51f60e47fd35d64956ff9445879a20c68160b954
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2014-07-07 12:08:59 +0700

    adding listing 5.7

---
+++ c5/listing-5.7.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+    Step = require('step');
+
+try {
+    Step (
+        function readData() {
+            fs.readFile('./data/data1.txt', 'utf8', this);
+        },
+        function modify(err, text) {
+            if (err) throw err;
+            return text.replace(/domain\.com/g, 'domain.ru');
+        },
+        function writeData(err, text) {
+            if (err) throw err;
+            fs.writeFile('./data/data1res.txt', text, this);
+        }
+    );
+} catch (err) {
+    console.error(err);
+}
