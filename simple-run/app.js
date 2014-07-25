b526ebb0adc1524052fd3735c55fe4a99f501d86
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2014-07-25 16:25:54 +0700

    experiments with expressjs

--- simple-run/app.js
+++ simple-run/app.js
@@ -19,10 +19,13 @@ app.set('view engine', 'ejs');
 
 
 var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 //var VelaStore = require('./tt_modules/penton-connect-vela')(session);
-//var velaOptions = {client: session.Cookie};
-// new VelaStore(velaOptions)
-app.use(session({secret: 'keyboard cat', store: new session.MemoryStore}));
+
+var redis = require('redis');
+var options = {client: redis.createClient(null, null, {detect_buffers: true})}; //session.MemoryStore
+
+app.use(session({secret: 'keyboard cat', store: new RedisStore(options)}));
 
 
 
@@ -34,13 +37,15 @@ app.use(bodyParser.urlencoded());
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var raven = require('raven');
-app.use(raven.middleware.express(
-    'https://9c499c71e92d438cb973f753f9e29156:f5089c54c7944d509045a0843b3262a1@app.getsentry.com/15563'
-));
 
 app.use('/', routes);
 app.use('/users', users);
+
+var raven = require('raven');
+
+app.use(raven.middleware.express(
+    'https://9c499c71e92d438cb973f753f9e29156:f5089c54c7944d509045a0843b3262a1@app.getsentry.com/15563'
+));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
