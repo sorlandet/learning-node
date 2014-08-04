04e2bace44e5a96bb02019f6cb49118a30940110
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2014-08-04 15:47:37 +0700

    experiments with expressjs

--- simple-run/routes/index.js
+++ simple-run/routes/index.js
@@ -1,14 +1,14 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    'use strict';
     res.render('index', { title: 'Express' });
 });
 
-router.get('/error', function mainHandler(req, res) {
-    'use strict';
+router.get('/error', function mainHandler() {
     throw new Error('Broke!');
 });
 
