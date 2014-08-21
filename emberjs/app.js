bc6e0dc8ddfd5b5a283d589d9fd6c79189eefb00
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2014-08-21 22:23:43 +0700

    experiments with ember.js (level 4)

--- emberjs/app.js
+++ emberjs/app.js
@@ -7,21 +7,21 @@ var App = Ember.Application.create({
 App.Router.map(function(){
     this.route('about', {path: '/about'});
     this.resource('products', function(){
-        this.resource('product', {path: '/:title'});
+        this.resource('product', {path: '/:product_id'});
     });
 
 });
 
 App.ProductsRoute = Ember.Route.extend({
     model: function(){
-        return App.PRODUCTS;
+        return this.store.findAll('product');
     }
 });
 
 App.ProductRoute = Ember.Route.extend({
     model: function(params){
         console.log(params);
-        return App.PRODUCTS.findBy('title', params.title);
+        return this.store.find('title', params.product_id);
     }
 });
 
@@ -34,19 +34,58 @@ App.IndexController = Ember.Controller.extend({
 });
 
 
-App.PRODUCTS = [
+//App.ApplicationAdapter = DS.RESTAdapter.extend({
+//
+//});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend({
+
+});
+
+App.Product = DS.Model.extend({
+    title: DS.attr('string'),
+    description: DS.attr('string'),
+    price: DS.attr('nubmer'),
+    isOnSale: DS.attr('boolean'),
+    image: DS.attr('string'),
+    reviews: DS.hasMany('review', {async: true})
+});
+
+App.Review = DS.Model.extend({
+    text: DS.attr('string'),
+    reviewedAt: DS.attr('date'),
+    product: DS.belongsTo('product')
+});
+
+App.Product.FIXTURES = [
     {
+        id: 1,
         title: 'Flint',
         description: 'bla bla bla',
         price: 99,
         isOnSale: true,
-        image: 'static/images/1.jpg'
+        image: 'static/images/1.jpg',
+        reviews: [100, 101]
     },
     {
+        id: 2,
         title: 'Kindling',
         description: 'bla bla bla',
         price: 249,
         isOnSale: false,
         image: 'static/images/2.jpg'
+    }
+];
+
+App.Review.FIXTURES = [
+    {
+        id: 100,
+        product: 1,
+        text: 'review 1 text'
+    },
+    {
+        id: 101,
+        product: 1,
+        text: 'review 2 text'
     }
 ];
