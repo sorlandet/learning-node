e277d2e44f76ccd52316b15344aa29797cfcc0f3
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2014-08-21 17:03:47 +0700

    experiments with ember.js (level 3 - in the middle)

--- emberjs/app.js
+++ emberjs/app.js
@@ -5,13 +5,46 @@ var App = Ember.Application.create({
 });
 
 App.Router.map(function(){
-    this.route('about', {path: '/aboutus'});
+    this.route('about', {path: '/about'});
+    this.resource('products');
+    this.resource('product', {path: '/products/:title'});
+});
+
+App.ProductsRoute = Ember.Route.extend({
+    model: function(){
+        return App.PRODUCTS;
+    }
+});
+
+App.ProductRoute = Ember.Route.extend({
+    model: function(params){
+        console.log(params);
+        return App.PRODUCTS.findBy('title', params.title);
+    }
 });
 
 App.IndexController = Ember.Controller.extend({
     productsCount: 6,
-    logo: 'static/images/logo.jpg',
+    logo: 'static/images/1.jpg',
     time: function(){
         return (new Date()).toDateString();
     }.property()
 });
+
+
+App.PRODUCTS = [
+    {
+        title: 'Flint',
+        description: 'bla bla bla',
+        price: 99,
+        isOnSale: true,
+        image: 'static/images/1.jpg'
+    },
+    {
+        title: 'Kindling',
+        description: 'bla bla bla',
+        price: 249,
+        isOnSale: false,
+        image: 'static/images/2.jpg'
+    }
+];
